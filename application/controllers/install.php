ca73157b73232f623bf7970b0a98ff8cf0220b5c
Xrong <taglete@gmail.com>
2013-03-13 09:46:58 +0800

    Just a test comment

--- application/controllers/install.php
+++ application/controllers/install.php
@@ -11,6 +11,7 @@ class Install extends CI_Controller
     {
         error_reporting(E_ALL);
         //使用数据库维护类的时候，无法指定timestamp的默认类型为CURRENT_TIMESTAMP
+        //Just a test comment
         $query = "create table `sites` (
                     `site_id` int(10) not null auto_increment,
                     `url` varchar(255) not null,
