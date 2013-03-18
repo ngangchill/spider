08847bf2b4eeeac65ad7073d129c152d96755a32
Xronger <taglete@gmail.com>
2013-03-18 16:49:11 +0800

    完成解析页面链接的正则

--- application/libraries/Crawling.php
+++ application/libraries/Crawling.php
@@ -19,10 +19,21 @@ class Crawling
 
     public function filterUrls($content)
     {
-        $reg = '|<a.*href="(.*)".*>.*</[^>]+>|U';
+        $reg = '|<a.*href="(http://mobile.163.com/\d{2}/\d{4}/\d{2}/\w{16}.html)".*>(.*)</a>|U'; //过滤出所有a标签
+        // $reg = '|http://mobile.163.com/\d{2}/\d{4}/\d{2}/\w{16}.html|U'; //过滤所有文章页
         if (preg_match_all($reg, $content, $results))
         {
-            return $results[1];
+            // return $results;
+            return array_combine($results[1], $results[2]);
+        }
+    }
+
+    public function getTitle($url)
+    {
+        $reg = '|<title>(.*)</title>|';
+        if (preg_match($reg, $this->getUrlContent($url), $result))
+        {
+            return $result[1];
         }
     }
 }
